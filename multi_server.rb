51cd0e39fa4819e20d35eda4c1cadec30bca857d
Dzirt232 <dzirt232@rambler.ru>
2017-11-01 21:29:36 +0300

    Web browser may send request(GET) and take responds.

--- multi_server.rb
+++ multi_server.rb
@@ -1,7 +1,7 @@
 require 'socket'                 # Get sockets from stdlib
 
 def parsing_request(request)
-  array_values = request.match(/^(\w+)\s+\/(\w+\.\w+)?\s+HTTP\/(\d+\.?\d+)/)
+  array_values = request.match(/^(\w+)\s+\/(\w+\.?\w+)?\s+HTTP\/(\d+\.?\d+)/)
   $function = array_values[1]
   $file_name = array_values[2]
   $http_version = array_values[3]
@@ -11,24 +11,23 @@ server = TCPServer.open(2000)    # Socket to listen on port 2000
 loop {                           # Servers run forever
    Thread.start(server.accept) do |client|
     request = client.gets
-    puts request
     parsing_request(request)
     if $function == "GET" && $file_name != nil
       exist_file = File.exists?($file_name)
       status = exist_file ? "HTTP/#{$http_version} 200 OK" : "HTTP/#{$http_version} 404 Not Found"
       if exist_file
         response = %{#{status}
-                      Date: #{Time.now.ctime}
-                      Content-Type: text/html
-                      Content-Length: #{File.size($file_name)}
+Date: #{Time.now.ctime}
+Content-Type: text/html
+Content-Length: #{File.size($file_name)}
 
-                    #{File.read($file_name)}}
+#{File.read($file_name)}}
       elsif !exist_file
         response = %{#{status}
-                      Content-Type: text/html
-                      Content-Length: #{File.size("not_found.html")}
+Content-Type: text/html
+Content-Length: #{File.size("not_found.html")}
 
-                    #{File.read("not_found.html")}}
+#{File.read("not_found.html")}}
       end
       client.print(response)
     else
--- web_browser.rb
+++ web_browser.rb
@@ -1,15 +1,31 @@
 require 'socket'
 
-host = 'www.tutorialspoint.com'     # The web server
-port = 80                           # Default HTTP port
-path = "/index.htm"                 # The file we want
+hostname = "localhost"
+port = 2000
 
-# This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
-
-socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read              # Read complete response
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2)
-print body                          # And display it
+  while true do
+    socket = TCPSocket.open(hostname,port)  # Connect to server
+    puts %{Enter your action:
+1: take a file
+2: close_connection}
+    case gets.chomp.strip.to_i
+    when 1
+      puts "Enter a file name:"
+      $file_name = gets.chomp.strip
+      request = %{GET /#{$file_name} HTTP/1.1
+}
+      socket.print(request)
+      response = socket.read
+      body = response.split("\n\n")[1]
+      puts
+      print body
+      puts
+      socket.close
+    when 2
+      puts "Closing the connection..."
+      socket.close
+      break
+    else
+      puts "Try again"
+    end
+end
